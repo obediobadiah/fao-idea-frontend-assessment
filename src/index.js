4266dbaeeaee82210043f7ff0fb7cd2b3b5114c0
obediobadiah <obediobadiah05@gmail.com>
2025-04-14 11:30:05 +0200

    [initialize react query client]

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Dashboard from './components/Dashboard';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <Dashboard />
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
